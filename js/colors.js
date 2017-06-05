db88b47c6f3b43638ad010bcd0e934c88215a71f
Molly Arant <mccormick.mollyt@gmail.com>
2017-06-05 19:54:19 -0400

    fixed code

--- js/colors.js
+++ js/colors.js
@@ -1,5 +1,5 @@
-var colorsArray : {
-    colors=[
+var colorsArray = {
+    colors:[
         {
             "name":"green",
             "red":"64",
@@ -10,24 +10,25 @@ var colorsArray : {
             "name":"yellow",
             "red":"239",
             "green":"183",
-            "blue":"62"
+            "blue":"62",
         },
         {
             name:"red",
             red:"192",
             green:"46",
-            blue:"35"
-        }
-        {
-            "name":"blue",
-            "red":"19",
-            "green":"71",
-            "blue":"144",
+            blue:"35",
         },
         {
-            "name"="orange",
-            "red"="253",
-            "green"="92",
-            "blue"="48"
+            name:"blue",
+            red:"19",
+            green:"71",
+            blue:"144",
+        },
+        {
+            name:"orange",
+            red:"253",
+            green:"92",
+            blue:"48"
         },
     ]
+}
